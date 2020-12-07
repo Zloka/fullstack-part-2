66a8e967eaf447bcee6390889a0cb196d7e7eb1a
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-07 19:40:00 +0200

    Implement a filter
    This marks my answer for exercise 2.9.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleAddPerson = (event) => {
     event.preventDefault();
@@ -28,9 +29,17 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleAddPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -43,7 +52,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => {
+      {persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())).map(person => {
         return <div key={person.name}>{person.name} {person.number}</div>
       })}
     </div>
