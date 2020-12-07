0ace39509318f179a2671b8135314f409d13ed7c
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-07 21:55:23 +0200

    Fetch and render weather data
    This marks my answer for exercise 2.14.

--- src/Country.js
+++ src/Country.js
@@ -1,4 +1,17 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Country = ({ country }) => {
+  const [weatherData, setWeatherData] = useState();
+  const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
+  useEffect(() => {
+    axios.get(`http://api.weatherstack.com/current?access_key=${WEATHER_API_KEY}&query=${country.capital}`)
+      .then(resp => {
+        setWeatherData(resp.data.current);
+      })
+  }, [country, WEATHER_API_KEY])
+
   return (
     <div>
       <h2>{country.name}</h2>
@@ -11,6 +24,24 @@ const Country = ({ country }) => {
         })}
       </ul>
       <img src={country.flag} width={200} height={100} alt="Failed to load flag" />
+      {weatherData ? (
+        <>
+        <h3>Weather in {country.capital}</h3>
+        <div>
+          <b>
+            temperature: 
+          </b>
+          {` ${weatherData.temperature} Celcius`}
+        </div>
+        {weatherData.weather_icons.length > 0 ? <div><img src={weatherData.weather_icons[0]} alt="Failed to load" /></div> : null}
+        <div>
+          <b>
+            wind: 
+          </b>
+          {` ${weatherData.wind_speed} mph direction ${weatherData.wind_dir}`}
+        </div>
+        </>
+      ) : null}
     </div>
   );
 }
