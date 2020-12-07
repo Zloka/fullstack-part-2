e70d5f690cfc8dab10d5c673c22d705bb2a751b0
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-07 19:28:45 +0200

    Implement the start of the phonebook
    This marks my answer for exercise 2.6.

--- src/App.js
+++ src/App.js
@@ -1,61 +1,38 @@
-import Course from './Course'
+import React, { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleAddPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName('');
+  }
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
-      <h1>Web development curriculum</h1>
-      {courses.map(course => {
-        return (
-          <Course key={course.id} course={course} />
-        );
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => {
+        return <div key={person.name}>{person.name}</div>
       })}
     </div>
   )
 }
 
-export default App;
+export default App
