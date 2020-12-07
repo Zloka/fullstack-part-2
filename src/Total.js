dc48614374193f794f8cb90d8da999abac5ec092
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-07 19:12:39 +0200

    Add separate <Course /> component
    Also added the "redux" entry, and left the total in (with changed text)
    since I had already done it using .reduce to begin with.
    
    This thus marks my answer for 2.1, 2.2 and 2.3.

--- src/Total.js
+++ src/Total.js
@@ -1,5 +1,11 @@
 const Total = ({ parts }) => {
-  return <p>Number of exercises {parts.reduce((acc, cur) => acc + cur.exercises, 0)}</p>
+  return (
+    <p>
+      <b>
+        total of {parts.reduce((acc, cur) => acc + cur.exercises, 0)} exercises
+      </b>
+    </p>
+  );
 };
 
 export default Total;
