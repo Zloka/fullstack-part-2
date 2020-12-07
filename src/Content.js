084ff6d29b4d7500e3a329cf98158204b6c84ef5
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-07 19:16:02 +0200

    Handle an arbitrary number of courses
    Changed id prop to be used rather than name when mapping out the <Part />
    componenents as well.
    
    Since my components are already separate modules, this marks my answer
    for 2.4 and 2.5.

--- src/Content.js
+++ src/Content.js
@@ -2,7 +2,7 @@ import Part from './Part';
 
 const Content = ({ parts }) => {
   return parts.map(part => {
-    return <Part key={part.name} part={part} />
+    return <Part key={part.id} part={part} />
   });
 };
 
