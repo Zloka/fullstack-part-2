31caacdd5d68f5c47cc94adb9ef6d7ef6dc143ba
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-07 20:00:22 +0200

    Fetch the persons from the json server
    This marks my answer for exercise 2.11.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+  const [ persons, setPersons ] = useState([]); 
   const [ newName, setNewName ] = useState('')
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(resp => {
+      setPersons(resp.data);
+    })
+  }, []);
 
   const handleAddPerson = (event) => {
     event.preventDefault();
